223a219c9740b856ee56d407a49e860edfc92cfe
Thinushi <thinupavani@gmail.com>
2022-01-04 16:19:06 +0530

    todo list ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import UsersPage from "./modules/UsersPage";
 import configureStore from "./redux/store/configureStore";
+import 'antd/dist/antd.css';
 
 const store = configureStore();
 
--- src/modules/UsersPage.jsx
+++ src/modules/UsersPage.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loadUserAsync } from '../redux/reducers/thunks'
+import { Col, Layout, Radio, Row, Space } from "antd";
+import { Content, Footer, Header } from "antd/lib/layout/layout";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUserAsync } from "../redux/reducers/thunks";
 
 const UsersPage = () => {
+  const dispatch = useDispatch();
+  const { isLoading, todos, errrorMessage } = useSelector(
+    (state) => state.todos
+  );
 
-    const dispatch = useDispatch()
-    const {isLoading, todos, errrorMessage} = useSelector( (state) => state.todos);
+  useEffect(() => {
+    dispatch(loadUserAsync());
+  }, []);
 
-    useEffect(() => {
-        dispatch(loadUserAsync());
-    },[]);
+  return (
+    <>
+      <Layout>
+        <Header>
+          <h1 style={{ color: "#fff", textAlign: "center" }}>User Listing</h1>
+        </Header>
 
-    return (
-        <div>
-            <h1>User Listing</h1>
-            { isLoading && <h3>Loading...</h3>}
+        <Content>
+          {/* use antd loading icon */}
+          {isLoading && <h3>Loading...</h3>}
+          {/* use antd alert */}
+          {errrorMessage && <h3>{errrorMessage}</h3>}
 
+          {/* use antd data display */}
+          
+          <Row>
+            <Col span={12} offset={6}>
+            
+            {todos &&
+            todos.map((todo) => 
+                <Radio key={todo.userId} >{todo.title}</Radio>
+            )}
+                
+            
+            </Col>
+          </Row>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </>
+  );
+};
 
-
-
-            {errrorMessage && <h3>{errrorMessage}</h3>}
-            {todos && todos.map((todo) => <h5 key={todo.userId}>{todo.title }</h5>)}
-        </div>
-    )
-}
-
-export default UsersPage
+export default UsersPage;
